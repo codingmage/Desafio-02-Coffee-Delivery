2750a70514dc3dcc886adb15b499356c3196f988
Jean Fellipe <jeanfellipesilva93@gmail.com>
2023-01-03 17:59:31 -0300

    start Checkout html

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,21 +1,85 @@
 import {
+  AddressInput,
   CheckoutContainer,
   CoffeeBought,
-  OrderContainer,
   OrderDetail,
+  PaymentDetail,
 } from './styles'
+
+import { CurrencyDollar, MapPinLine } from 'phosphor-react'
 
 export function Checkout() {
   return (
     <CheckoutContainer>
-      <OrderContainer>
-        <h3>Complete seu pedido</h3>
+      <div>
+        <h2>Complete seu pedido</h2>
         <OrderDetail>
-          <div></div>
+          <header>
+            <span className="icon">
+              <MapPinLine size={22} />
+            </span>
+            <div>
+              <h3>Endereço de Entrega</h3>
+              <p>Informe o endereço onde deseja receber seu pedido</p>
+            </div>
+          </header>
+          <AddressInput>
+            <form action="">
+              <input
+                type="text"
+                placeholder="CEP"
+                className="firstBox"
+                required
+              />
+              <input type="text" placeholder="Rua" className="rua" required />
+              <div>
+                <input
+                  type="text"
+                  placeholder="Número"
+                  className="firstBox"
+                  required
+                />
+                <input
+                  type="text"
+                  placeholder="Complemento"
+                  className="complemento"
+                />
+              </div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="Bairro"
+                  className="firstBox"
+                  required
+                />
+                <input
+                  type="text"
+                  placeholder="Cidade"
+                  className="cidade"
+                  required
+                />
+                <input type="text" placeholder="UF" className="last" required />
+              </div>
+            </form>
+          </AddressInput>
         </OrderDetail>
-      </OrderContainer>
+        <PaymentDetail>
+          <header>
+            <span className="icon">
+              <CurrencyDollar size={22} />
+            </span>
+            <div>
+              <h3>Pagamento</h3>
+              <p>
+                O pagamento é feito na entrega. Escolha a forma que deseja
+                pagar.
+              </p>
+            </div>
+          </header>
+        </PaymentDetail>
+      </div>
       <CoffeeBought>
-        <h3>Cafés selecionados </h3>
+        <h2>Cafés selecionados </h2>
         <div></div>
       </CoffeeBought>
     </CheckoutContainer>
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -2,20 +2,123 @@ import styled from 'styled-components'
 
 export const CheckoutContainer = styled.div`
   display: flex;
-`
+  gap: 0.75rem;
+  margin-left: 10rem;
+  margin-right: 10rem;
+  font-family: 'Roboto', sans-serif;
+  font-size: 0.875rem;
 
-export const OrderContainer = styled.div``
+  h2 {
+    font-family: 'Baloo 2', sans-serif;
+    font-size: 1.125rem;
+    color: ${(props) => props.theme['base-subtitle']};
+  }
 
-export const OrderDetail = styled.div`
-  div {
-    width: 16rem;
-    height: 17.3rem;
-    background-color: ${(props) => props.theme['base-card']};
+  h3 {
+    font-size: 1rem;
+    font-weight: 400;
+    color: ${(props) => props.theme['base-subtitle']};
+  }
+
+  p {
+    color: ${(props) => props.theme['base-text']};
+  }
+
+  header {
+    display: flex;
+  }
+
+  header span {
+    margin-right: 0.715rem;
+  }
+
+  header div {
     display: flex;
     flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
-    border-radius: 6px 36px;
+  }
+`
+
+export const OrderDetail = styled.div`
+  width: 40rem;
+  background-color: ${(props) => props.theme['base-card']};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  border-radius: 6px 36px;
+  height: fit-content;
+  margin-top: 0.938rem;
+  padding: 2.5rem;
+  margin-bottom: 2rem;
+
+  .icon {
+    color: ${(props) => props.theme['yellow-dark']};
+  }
+`
+
+export const AddressInput = styled.div`
+  margin-top: 2rem;
+  width: 100%;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  div {
+    display: flex;
+    flex: auto;
+    flex-grow: 1;
+    gap: 0.75rem;
+    justify-content: space-between;
+  }
+
+  .cidade {
+    flex-grow: 1;
+  }
+
+  .complemento {
+    flex-grow: 1;
+  }
+
+  .firstBox {
+    width: 12.5rem;
+  }
+
+  .last {
+    width: 3.75rem;
+  }
+
+  input {
+    background-color: ${(props) => props.theme['base-button']};
+    font-family: 'Roboto', sans-serif;
+    color: ${(props) => props.theme['base-text']};
+    font-weight: 400;
+    border: 1px solid #e6e5e5;
+    border-radius: 4px;
+    box-sizing: border-box;
+    padding: 0.75rem;
+  }
+
+  input::placeholder {
+    color: ${(props) => props.theme['base-label']};
+  }
+`
+
+export const PaymentDetail = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 40rem;
+  background-color: ${(props) => props.theme['base-card']};
+  border-radius: 6px 36px;
+  height: fit-content;
+  margin-top: 0.938rem;
+  padding: 2.5rem;
+  margin-bottom: 2rem;
+
+  .icon {
+    color: ${(props) => props.theme.purple};
   }
 `
 
