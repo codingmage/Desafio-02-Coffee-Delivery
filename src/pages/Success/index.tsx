df207c4a0a824d96a3aa72c67c47063af640344e
Jean Fellipe <jeanfellipesilva93@gmail.com>
2023-01-07 15:17:08 -0300

    add Success

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,5 +1,49 @@
-import { SuccessContainer } from './styles'
+import { AddressContainer, MainContainer, SuccessContainer } from './styles'
+import Scooter from '../../assets/Illustration.png'
+import { Timer, CurrencyDollar, MapPin } from 'phosphor-react'
 
 export function Success() {
-  return <SuccessContainer></SuccessContainer>
+  return (
+    <SuccessContainer>
+      <h2>Uhu! Pedido confirmado</h2>
+      <p>Agora é só aguardar que logo o café chegará até você</p>
+      <MainContainer>
+        <div className="border">
+          <AddressContainer>
+            <div className="text-container">
+              <span className="icon pin">
+                <MapPin size={16} weight="fill" />
+              </span>
+              <div>
+                Entrega em
+                <span className="bold-address">
+                  Rua João Daniel Martinelli, 102
+                </span>
+                <p>Farrapos - Porto Alegre, RS</p>
+              </div>
+            </div>
+            <div className="text-container">
+              <span className="icon timer">
+                <Timer size={16} />
+              </span>
+              <div>
+                Previsão de entrega
+                <p className="bold-text">20 min - 30 min</p>
+              </div>
+            </div>
+            <div className="text-container">
+              <span className="icon currency">
+                <CurrencyDollar size={16} />
+              </span>
+              <div>
+                Pagamento na entrega
+                <p className="bold-text">Cartão de Crédito</p>
+              </div>
+            </div>
+          </AddressContainer>
+        </div>
+        <img src={Scooter} alt="Delivery Man" />
+      </MainContainer>
+    </SuccessContainer>
+  )
 }
--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -1,3 +1,93 @@
 import styled from 'styled-components'
 
-export const SuccessContainer = styled.div``
+export const SuccessContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 5rem 10rem;
+
+  h2 {
+    font-family: 'Baloo 2', sans-serif;
+    font-weight: 800;
+    font-size: 2rem;
+    color: ${(props) => props.theme['yellow-dark']};
+  }
+
+  p {
+    font-size: 1.25rem;
+    color: ${(props) => props.theme['base-subtitle']};
+  }
+`
+
+export const MainContainer = styled.main`
+  display: flex;
+  margin-top: 2.5rem;
+  gap: 6.375rem;
+  justify-content: space-around;
+
+  img {
+    width: 30.75rem;
+    height: 18.313rem;
+  }
+
+  .border {
+    padding: 0.75rem;
+    background: linear-gradient(to right, #dbac2c, #8047f8);
+    border-radius: 6px 36px;
+  }
+`
+
+export const AddressContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 2.5rem;
+  gap: 2rem;
+  font-size: 1rem;
+  color: ${(props) => props.theme['base-text']};
+  background-color: ${(props) => props.theme.background};
+  /*   border: 1px solid;
+  border-radius: 6px 36px;
+  border-image: linear-gradient(to right, #dbac2c, #8047f8) 1; */
+
+  p {
+    font-size: 1rem;
+  }
+
+  .bold-address {
+    font-weight: bold;
+    margin-left: 5px;
+  }
+
+  .bold-text {
+    font-weight: bold;
+  }
+
+  .text-container {
+    display: flex;
+    align-items: center;
+  }
+
+  .icon {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 2rem;
+    height: 2rem;
+    margin-right: 0.75rem;
+    padding: 0.5rem;
+    gap: 0.5rem;
+    border-radius: 999px;
+    color: ${(props) => props.theme.background};
+  }
+
+  .pin {
+    background-color: ${(props) => props.theme.purple};
+  }
+
+  .timer {
+    background-color: ${(props) => props.theme.yellow};
+  }
+
+  .currency {
+    background-color: ${(props) => props.theme['yellow-dark']};
+  }
+`
